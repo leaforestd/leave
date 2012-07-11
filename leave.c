6e2b8e1306daab8a352b1ed48903fedb9969c55f
leaforestd <leaforestd@gmail.com>
2012-07-11 17:44:54 +0800

    unsigned sleep_sec: seems no problem

--- leave.c
+++ leave.c
@@ -5,54 +5,64 @@
 #include <string.h>
 #include <errno.h>
 #include <time.h>
-
-#define MAXBUF 5 /* [+]hhmm */
+#include <ctype.h>
 
 void unix_error(char* msg);
 int Fork(void);
 char* Fgets(char* s, int size, FILE* stream);
+void invalid_input(void);
 
 void main(int argc, char* argv[]) {
 	static int loop = 0;
 	unsigned int sleep_sec;
 	pid_t pid;
-	int arg_buf[MAXBUF];
+	int arg_buf[5];
 	int index = 0;
+	int i, j;
 
 	if (argc == 1) { /* no arg */
 		int input;
 
 		printf("When do you have to leave?\n");
 		while ((input = getchar()) == ' ') {;}
-		/* first non-blank character */
-		if ((input == '\n') || (input == EOF)) /* blank line, terminate */
+		if ((input == '\n') || (input == EOF)) /* empty line */
 			return;
 		
-		while ((input != '\n') && (input != EOF) && (index < MAXBUF)) {
-			/* index: [0, 4] */
-			arg_buf[index++] = input;
+		while ((input != '\n') && (input != EOF) && (index < 5)) {
+			arg_buf[index++] = input; /* index: [0, 4] */
 			input = getchar();
 		}
-	} else { /* deal with arg */
-		int i;
-		for (i = 0; i < MAXBUF; i++) {
+
+	} else { /* have arg */
+		for (i = 0; i < 5; i++) {
 			arg_buf[i] = argv[1][i] - '0'; /* ASCII -> int */
 		}
 	}
 
-	if (arg_buf[0] == '+') {
-		int j;
-		for (j = 1; j < MAXBUF; j++) {
+	if (arg_buf[0] == '+') { /* +hhmm */
+		for (j = 1; j < 5; j++) {
 			arg_buf[j] -= '0';
+			/* check arg_buf */
+			if (arg_buf[j] < 0 || arg_buf[j] > 9)  
+				invalid_input();
 		}
 
-		sleep_sec = (arg_buf[1] * 10 + arg_buf[2]) * 3600 + (arg_buf[3] * 10 + arg_buf[4]) * 60;
+		sleep_sec = (arg_buf[1] * 10 + arg_buf[2]) * 3600 + (arg_buf[3] * 10 + arg_buf[4]) * 60; /* int -> unsigned, seems no problem? */
 		printf("sleep_sec: %d\n", sleep_sec);
-	} else {
-		if (arg_buf[0]<0 || arg_buf[0]>1 || arg_buf[1]<0 || arg_buf[1]>2 || arg_buf[2]<0 || arg_buf[2]>6 || arg_buf[3]<0 || arg_buf[3]>9) {
-			printf("invalid input, exit..\n");
-			return;
+
+	} else { /* hhmm */
+		for (j = 0; j < 5 - 1; j++) {
+			arg_buf[j] -= '0';
+			/* check arag_buf */
+			if (arg_buf[j] < 0 || arg_buf[j] > 9) 
+				invalid_input();
 		}
+
+		if (arg_buf[0]>2 || arg_buf[2]>6) {
+			invalid_input();
+
+		time_t time_sec = time(NULL);
+		struct tm *time_stc = localtime(&time_sec);
 	}
 
 	
@@ -69,7 +79,7 @@ void main(int argc, char* argv[]) {
 	}	
 	/* forced to leave */
 	printf("that's the last time I'll tell you. Bye.\n");
-	char* execve_argv[] = {"usr/sbin/shutdown", "now", NULL};
+	char* execve_argv[] = {"/usr/sbin/shutdown", "now", NULL};
 	char* execve_environ[] = {NULL};
 	execve(execve_argv[0], execve_argv, execve_environ);
 }
@@ -95,3 +105,7 @@ char* Fgets(char* s, int size, FILE* stream) {
 	return ptr;
 }
 
+void invalid_input(void) {
+	printf("leave: usage: leave [+]hhmm\n");
+	return;
+}
